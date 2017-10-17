30d65eb02005b5428c6f4b78eb46465f3542ccae
Piotr-Setsko <lihadzey@gmail.com>
2017-10-17 17:58:57 +0300

    Пробовал добавить маркер в ie

--- js/google.js
+++ js/google.js
@@ -1,14 +1,18 @@
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
+  var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 17,
     center: {lat: 59.938896, lng: 30.323089}
   });
 
-  var image = '../img/icon-map-pin.svg';
+  var image = {
+    url: "../img/icon-map-pin.svg",
+    scaledSize: new google.maps.Size(100, 100),
+  }
   var beachMarker = new google.maps.Marker({
     position: {lat: 59.938896, lng: 30.323089},
     title: 'г. Санкт-Петербург, \nул. Большая Конюшенная, \nд. 19/8, офис 101',
     map: map,
+    optimized: false,
     icon: image
   });
 }
