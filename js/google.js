9d710245ddec425bc115436dd873bb362e9b3f1d
Piotr-Setsko <lihadzey@gmail.com>
2017-10-17 17:43:02 +0300

    Добавил google карту

--- js/google.js
+++ js/google.js
@@ -1,12 +1,13 @@
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 10,
-    center: {lat: 59.93, lng: 30.32}
+    zoom: 17,
+    center: {lat: 59.938896, lng: 30.323089}
   });
 
   var image = '../img/icon-map-pin.svg';
   var beachMarker = new google.maps.Marker({
-    position: {lat: 59.93, lng: 30.32},
+    position: {lat: 59.938896, lng: 30.323089},
+    title: 'г. Санкт-Петербург, \nул. Большая Конюшенная, \nд. 19/8, офис 101',
     map: map,
     icon: image
   });
