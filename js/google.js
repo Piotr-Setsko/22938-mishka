08aa5ed9bfaf977f841694c3d4f7a93b4494255e
Piotr-Setsko <lihadzey@gmail.com>
2017-10-17 19:17:27 +0300

    Изменил google.js

--- js/google.js
+++ js/google.js
@@ -4,15 +4,12 @@ function initMap() {
     center: {lat: 59.938896, lng: 30.323089}
   });
 
-  var image = {
-    url: "../img/icon-map-pin.svg",
-    scaledSize: new google.maps.Size(100, 100),
-  }
+  var image = "../img/icon-map-pin.svg";
+
   var beachMarker = new google.maps.Marker({
     position: {lat: 59.938896, lng: 30.323089},
     title: 'г. Санкт-Петербург, \nул. Большая Конюшенная, \nд. 19/8, офис 101',
     map: map,
-    optimized: false,
     icon: image
   });
 }
