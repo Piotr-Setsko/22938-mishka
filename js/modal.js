09c6a766ee20563306421a2eca6f5ec18505cf8f
Piotr-Setsko <lihadzey@gmail.com>
2017-10-22 20:15:12 +0300

    Изменил названия в modal-order

--- js/modal.js
+++ js/modal.js
@@ -6,22 +6,22 @@
     for (var i = 0; i < link.length; i++) {
     link[i].addEventListener('click', function (evt) {
     evt.preventDefault();
-    popup.classList.add("modal-order__show");
-    overlay.classList.add("modal-order__overlay-show");
+    popup.classList.add("modal-order--show");
+    overlay.classList.add("modal-order__overlay--show");
   });
 }
 
   overlay.addEventListener("click", function(evt) {
       evt.preventDefault();
-      popup.classList.remove("modal-order__show");
-      overlay.classList.remove("modal-order__overlay-show");
+      popup.classList.remove("modal-order--show");
+      overlay.classList.remove("modal-order__overlay--show");
   });
 
   window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
-      if (popup.classList.contains("modal-order__show")) {
-        popup.classList.remove("modal-order__show");
-        overlay.classList.remove("modal-order__overlay-show");
+      if (popup.classList.contains("modal-order--show")) {
+        popup.classList.remove("modal-order--show");
+        overlay.classList.remove("modal-order__overlay--show");
       }
     }
   });
