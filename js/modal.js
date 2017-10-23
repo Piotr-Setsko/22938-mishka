a21b74f0fb2b33660c249c475a789e77c46a2bd8
Piotr-Setsko <lihadzey@gmail.com>
2017-10-23 14:49:46 +0300

    Добавил блок overlay

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,5 @@
   var popup = document.querySelector(".modal-order");
-  var overlay = document.querySelector(".modal-order__overlay");
+  var overlay = document.querySelector(".overlay");
 
     if (document.querySelectorAll(".modal-order__btn"))
     var link = document.querySelectorAll(".modal-order__btn");
@@ -7,21 +7,21 @@
     link[i].addEventListener('click', function (evt) {
     evt.preventDefault();
     popup.classList.add("modal-order--show");
-    overlay.classList.add("modal-order__overlay--show");
+    overlay.classList.add("overlay--show");
   });
 }
 
   overlay.addEventListener("click", function(evt) {
       evt.preventDefault();
       popup.classList.remove("modal-order--show");
-      overlay.classList.remove("modal-order__overlay--show");
+      overlay.classList.remove("overlay--show");
   });
 
   window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
       if (popup.classList.contains("modal-order--show")) {
         popup.classList.remove("modal-order--show");
-        overlay.classList.remove("modal-order__overlay--show");
+        overlay.classList.remove("overlay--show");
       }
     }
   });
